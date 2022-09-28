4e8a437d5a35d6c71ad8e269095abacb403e20ca
Camel Ninla <rfvbkm0220@gmail.com>
2022-09-28 20:26:14 +0300

    feat: s3 simple methods

--- cmd/cloudphoto/main.go
+++ cmd/cloudphoto/main.go
@@ -3,5 +3,3 @@ package main
 func main() {
 
 }
-git config --global user.email "you@example.com"
-git config --global user.name "Your Name"
---
+++ internal/usecase/client/adapter.go
@@ -0,0 +1,69 @@
+package client
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
+	"github.com/rs/zerolog"
+)
+
+type adapter struct {
+	logger *zerolog.Logger
+	client *s3.Client
+}
+
+func createClinet() *s3.Client {
+	// Создаем кастомный обработчик эндпоинтов, который для сервиса S3 и региона ru-central1 выдаст корректный URL
+	customResolver := aws.EndpointResolverWithOptionsFunc(func(service, region string, options ...interface{}) (aws.Endpoint, error) {
+		if service == s3.ServiceID && region == "ru-central1" {
+			return aws.Endpoint{
+				PartitionID:   "yc",
+				URL:           "https://storage.yandexcloud.net",
+				SigningRegion: "ru-central1",
+			}, nil
+		}
+		return aws.Endpoint{}, fmt.Errorf("unknown endpoint requested")
+	})
+
+	// Подгружаем конфигрурацию из ~/.aws/*
+	cfg, err := config.LoadDefaultConfig(context.TODO(), config.WithEndpointResolverWithOptions(customResolver))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Создаем клиента для доступа к хранилищу S3
+	client := s3.NewFromConfig(cfg)
+	return client
+}
+
+func loadDefaultConfig(client *s3.Client) {
+
+	// Запрашиваем список бакетов
+	result, err := client.ListBuckets(context.TODO(), &s3.ListBucketsInput{})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, bucket := range result.Buckets {
+		log.Printf("backet=%s creation time=%s", aws.ToString(bucket.Name), bucket.CreationDate.Format("2006-01-02 15:04:05 Monday"))
+	}
+
+}
+
+func listObject(client *s3.Client, bucketName *string) {
+	// Запрашиваем список бакетов
+	object, err := client.ListObjectsV2(context.TODO(), &s3.ListObjectsV2Input{
+		Bucket: aws.String(*bucketName),
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, object := range object.Contents {
+		log.Printf("object=%s size=%d Bytes last modified=%s", aws.ToString(object.Key), object.Size, object.LastModified.Format("2006-01-02 15:04:05 Monday"))
+	}
+}
