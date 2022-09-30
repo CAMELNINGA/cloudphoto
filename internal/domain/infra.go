a74eb8386d056cdd2e0e991f36b267fec4920195
Camel Ninla <rfvbkm0220@gmail.com>
2022-09-30 22:43:56 +0300

    feat: Client  interface

--- internal/domain/infra.go
+++ internal/domain/infra.go
@@ -1,5 +1,11 @@
 package domain
 
-type Client interface{
-	
+import "io"
+
+type Client interface {
+	Getobject(key string) ([]byte, error)
+	CreateBucket(name string) error
+	PutObject(file io.Reader, key string, size int64) error
+	ListObject() ([]string, error)
+	LoadDefaultConfig() ([]string, error)
 }
