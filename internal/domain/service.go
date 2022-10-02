d2e95c5c6c8472ca1bacd6a5b49a93e0ac04dafd
Camel Ninla <rfvbkm0220@gmail.com>
2022-10-02 19:35:18 +0300

    feat: Upload, List ,Delete

--- internal/domain/service.go
+++ internal/domain/service.go
@@ -65,6 +65,7 @@ func (s *service) Download(album, dir string) error {
 		}
 		if err := os.MkdirAll(filepath.Dir(object), 0770); err != nil {
 			fmt.Printf("Error creating dir %s \n", filepath.Dir(object))
+			break
 		}
 		wg.Add(1)
 
@@ -72,5 +73,136 @@ func (s *service) Download(album, dir string) error {
 	}
 
 	wg.Wait()
+	return nil
+}
+
+func (s *service) Upload(album, path string) error {
+	if path == "" {
+		path = "./"
+	}
+	files, err := FilePathWalkDir(path)
+	if err != nil {
+		fmt.Printf("Error scaning dir %s \n", err)
+		return err
+	}
+	op := func(path, file string) {
+		f, err := os.Open(path + file)
+		if err != nil {
+			fmt.Printf("Error open file %s \n", err)
+			continue
+		}
+		defer f.Close()
+		fi, err := f.Stat()
+		if err != nil {
+			fmt.Printf("Error open file %s \n", err)
+			continue
+		}
+		if err := s.client.PutObject(f, album+"/"+file, fi.Size()); err != nil {
+			fmt.Printf("Error put object %s \n", album+"/"+file)
+			continue
+		}
+	}
+
+	for _, file := range files {
+		op(path, file)
+	}
+	return err
+}
+
+func FilePathWalkDir(root string) ([]string, error) {
+	var files []string
+	err := filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
+		if !info.IsDir() {
+			files = append(files, path)
+		}
+		return nil
+	})
+	return files, err
+}
+
+func haveAlbum(objects []string, album string, a *regexp.Regexp) bool {
+	haveAlbum := false
+	for _, object := range objects {
+		a := a.Split(object, 2)
+		if album != a[0] && album != "" {
+			continue
+		}
+		haveAlbum = true
+	}
+	return haveAlbum
+}
+
+func (s *service) List(album string) error {
+	objects, err := s.client.ListObject()
+	if err != nil {
+		fmt.Printf("Error while getig objects %s \n", err)
+		return err
+	}
+	a := regexp.MustCompile(`/`)
+	haveAlbum := haveAlbum(objects, album, a)
+	if !haveAlbum {
+		fmt.Printf("Error don't find album %s \n", album)
+		return fmt.Errorf("error don't find album %s \n", album)
+	}
+	for _, object := range objects {
+		a := a.Split(object, 2)
+		if album != a[0] && album != "" {
+			continue
+		}
+		if len(a) != 1 {
+			fmt.Printf("Album %s Object %s ", a[0], a[1])
+		} else {
+			fmt.Printf("Object %s ", object)
+		}
+	}
+	return nil
+}
+
+func haveObject(objects []string, sobject string) bool {
+	haveAlbum := false
+	for _, object := range objects {
+
+		if object != sobject {
+			continue
+		}
+		haveAlbum = true
+	}
+	return haveAlbum
+}
+func (s *service) Delete(album, photo string) error {
+	objects, err := s.client.ListObject()
+	if err != nil {
+		fmt.Printf("Error while getig objects %s \n", err)
+		return err
+	}
+	var sobject string
+	if photo != "" {
+		sobject = album + "/" + photo
+		if !haveObject(objects, sobject) {
+			fmt.Printf("Error while searching objects %s \n", sobject)
+			return fmt.Errorf("error while searching objects %s \n", sobject)
+		}
+		if err = s.client.DeleteObject(sobject); err != nil {
+			fmt.Printf("Error while deleting objects %s \n", sobject)
+			return fmt.Errorf("error while deleting objects %s \n", sobject)
+		}
+		return nil
+	}
+	sobject = album
+	a := regexp.MustCompile(`/`)
+	if !haveAlbum(objects, sobject, a) {
+		fmt.Printf("Error while searching objects %s \n", sobject)
+		return fmt.Errorf("error while searching objects %s \n", sobject)
+	}
+	for _, object := range objects {
+		a := a.Split(object, 2)
+		if album != a[0] && album != "" {
+			continue
+		}
+		if err = s.client.DeleteObject(object); err != nil {
+			fmt.Printf("Error while deleting objects %s \n", object)
+			return fmt.Errorf("error while deleting objects %s \n", object)
+		}
+	}
 	return nil
 }
